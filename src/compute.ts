e78784aa5f6487fa5e184902eaf49d306b120bb3
MarijnV <marijnverstraete@gmail.com>
2020-10-05 15:20:21 +0200

    ready to merge to master

--- src/compute.ts
+++ src/compute.ts
@@ -2,8 +2,6 @@ import {Game, LastFrame} from "./types";
 
 export function compute(game: Game): number {
   let score:number = 0;
-  let wasSpare:boolean=false;
-  let wasStrike:boolean=false;
 
   for (let frame of game) {
     score += frame[0] + frame[1];
