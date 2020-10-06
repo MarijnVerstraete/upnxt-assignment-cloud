ed2a6f5f6a17ac33c49c1c7385a6f1ecb76e6955
MarijnV <marijnverstraete@gmail.com>
2020-10-06 15:35:56 +0200

    refactored validation woops

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import http from "http";
 import express from "express";
 import { compute } from "./compute";
-import {Game} from "./types";
+import {Frame, Game, LastFrame} from "./types";
 
 const app = express();
 
@@ -21,48 +21,52 @@ app.post("/compute", (request, response) => {
 
 });
 
-function isValidGame(object:any) : boolean {
-  if (
-      Array.isArray(object)
+//TODO make validation throw errors to catch in a try-catch, passing the error message in the body of the response
+function isValidGame(object:Game) : boolean {
+  return Array.isArray(object)
       && object.length === 10
-  ) {
-
-    for ( let element of object ) {
-      let lastElement = object[object.length - 1];
-
-      if (
-          Array.isArray(element)
-          && ( element.length === 2 && element !== lastElement )
-          || ( element.length === 3 && element === lastElement )
-      ) {
-
-        for ( let entry of element ) {
-          if (
-              typeof entry === "number"
-              && entry >= 0
-              && entry <=10
-              && entry % 1 === 0
-          ) {
-          } else return false;
-        }
-
-        let sum = element.reduce( (a, b) => a + b, 0 );
-        if (
-            ( element !== lastElement && sum <= 10 )
-            || (
-                element === lastElement
-                && ( element[0] !== 10 && (element[0] + element[1]) === 10 )
-                || ( element[0] === 10 && sum <= 30 )
-            )
-        ) {} else return false;
-
-      } else return false;
-
-    }
-
-  } else return false;
-
-  return true
+      && framesAreArraysAndLengthsOk(object)
+      && object.every(individualPinNumberOk)
+      && object.every(totalNumberOfPinsOk);
 }
+
+function framesAreArraysAndLengthsOk (object) : boolean {
+  const lastElement = object[object.length - 1];
+
+  for ( let element of object ) {
+    if ( ! (
+        Array.isArray(element)
+        && ( element.length === 2 && element !== lastElement )
+        || ( element.length === 3 && element === lastElement )
+    ) ) return false;
+  }
+  return true;
+}
+
+function individualPinNumberOk (frame: Frame | LastFrame) : boolean {
+  return frame.every(isValidPinAmount);
+}
+
+function isValidPinAmount (amountOfPins) : boolean {
+  return typeof amountOfPins === "number"
+      && amountOfPins >= 0
+      && amountOfPins <=10
+      && amountOfPins % 1 === 0;
+}
+
+
+function totalNumberOfPinsOk(frame: Frame | LastFrame) : boolean {
+  const sumOfPins = frame.reduce( (a, b) => a + b, 0 );
+
+  if (frame.length === 2) {
+    return sumOfPins <= 10;
+  } else if (frame.length === 3 ) {
+    return ( frame[0] !== 10 && (frame[0] + frame[1]) === 10 )
+        || ( frame[0] === 10 && sumOfPins <= 30 );
+  } else {
+    return false;
+  }
+}
+
 
 export const createServer = () => http.createServer(app);
